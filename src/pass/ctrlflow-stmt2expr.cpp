10dd24c2040d8e5011f196fc962f50d01c956632
PENGUINLIONG <admin@penguinliong.moe>
2022-05-05 02:26:54 +0800

    tep

--- src/pass/ctrlflow-stmt2expr.cpp
+++ src/pass/ctrlflow-stmt2expr.cpp
@@ -16,65 +16,79 @@ bool func_var_eq(
 
 struct FunctionVariableRecord {
   MemoryFunctionVariableRef func_var;
-  size_t scope_idx; // Index in `scope_stack`.
+  uint32_t scope_lv;
   ExprRef value;
 };
 struct ScopeRecord {
-  size_t idx;
-  std::set<MemoryFunctionVariableRef> stored_vars;
+  uint32_t scope_lv;
+  std::vector<StmtRef> prelude;
   std::vector<FunctionVariableRecord> func_vars;
-
-  FunctionVariableRecord& get_func_var(const MemoryFunctionVariableRef& func_var) {
+  bool is_within_loop;
+  
+  bool try_get_func_var(
+    const MemoryFunctionVariableRef& func_var,
+    FunctionVariableRecord*& out
+  ) {
     auto it = std::find_if(
       func_vars.begin(),
       func_vars.end(),
       [&](const FunctionVariableRecord& else_var) {
         return func_var->structured_eq(else_var.func_var);
       });
-    assert(it != func_vars.end());
-    return *it;
+    if (it != func_vars.end()) {
+      out = &*it;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  FunctionVariableRecord& get_func_var(const MemoryFunctionVariableRef& func_var) {
+    FunctionVariableRecord* record = nullptr;
+    assert(try_get_func_var(func_var, record));
+    return *record;
   }
   void set_func_var(const MemoryFunctionVariableRef& func_var, const ExprRef& value) {
+    FunctionVariableRecord* record = nullptr;
+    if (try_get_func_var(func_var, record)) {
+      record->value = value;
+    } else {
+      FunctionVariableRecord record {};
+      record.func_var = func_var;
+      record.value = value;
+      func_vars.emplace_back(std::move(record));
+    }
+  }
+  void remove_func_var(const MemoryFunctionVariableRef& func_var) {
     auto it = std::find_if(
       func_vars.begin(),
       func_vars.end(),
       [&](const FunctionVariableRecord& else_var) {
         return func_var->structured_eq(else_var.func_var);
       });
-    if (it == func_vars.end()) {
-      FunctionVariableRecord record {};
-      record.func_var = func_var;
-      record.scope_idx = idx;
-      record.value = value;
-      func_vars.emplace_back(std::move(record));
-    } else {
-      it->value = value;
+    if (it != func_vars.end()) {
+      func_vars.erase(it);
     }
   }
 };
 
 struct CtrlflowStmt2ExprMutator : public Mutator {
-  std::vector<ScopeRecord> scope_stack { { 1 } };
 
+  std::vector<ScopeRecord> scope_stack { {} };
   ScopeRecord& get_scope() {
     return scope_stack.back();
   }
 
-  void push_scope() {
+  void push_scope(bool is_within_loop) {
     ScopeRecord record {};
-    record.idx = scope_stack.size() + 1;
-    record.stored_vars = scope_stack.back().stored_vars;
+    record.scope_lv = scope_stack.back().scope_lv + 1;
+    record.func_vars = scope_stack.back().func_vars;
+    record.is_within_loop = is_within_loop || scope_stack.back().is_within_loop;
     scope_stack.emplace_back(std::move(record));
   }
-  std::vector<StmtRef> pop_scope() {
-    std::vector<StmtRef> out;
-    for (const auto& func_var : scope_stack.back().stored_vars) {
-      const FunctionVariableRecord& func_var_record = get_scope().get_func_var(func_var);
-      MemoryFunctionVariableRef func_var = func_var_record.func_var;
-      StmtRef stmt = new StmtStore(func_var, func_var_record.value);
-    }
+  ScopeRecord pop_scope() {
+    auto record = std::move(scope_stack.back());
     scope_stack.pop_back();
-    return out;
+    return record;
   }
 
 
@@ -83,16 +97,39 @@ struct CtrlflowStmt2ExprMutator : public Mutator {
   virtual ExprRef mutate_expr_(ExprLoadRef x) override final {
     if (x->src_ptr->is<MemoryFunctionVariable>()) {
       MemoryFunctionVariableRef src_ptr = x->src_ptr;
-      const auto& func_var_record = get_scope().get_func_var(src_ptr);
-
-      if (scope_stack.size() != func_var_record.scope_idx) {
-        scope_stack.back().stored_vars.insert(src_ptr);
-        return x;
+      FunctionVariableRecord* func_var_record = nullptr;
+      if (get_scope().try_get_func_var(src_ptr, func_var_record)) {
+        if (func_var_record->scope_lv < get_scope().scope_lv) {
+          // The variable got its value from an outer scope. In case of loops, the value might change over time if it's referenced within iterations.
+          get_scope().prelude.emplace_back(new StmtStore(src_ptr, func_var_record->value));
+          get_scope().remove_func_var(src_ptr);
+          return x;
+        } else if (func_var_record->scope_lv == get_scope().scope_lv) {
+          // This value is make within the scope. In terms of loops, the value is assigned in this current iteration.
+          return func_var_record->value;
+        } else {
+          // The variable is created by an inner scope? It should not happen and these inner variables should be carried out by algorithms.
+          unreachable();
+        }
       } else {
-        return func_var_record.value;
+        // The only case a function variable cannot be found is that, it's been removed because its created by an outer scope and the variable has been loaded before.
+        // Otherwise the variable is never assigned even in the original program, and there is nothing we can help with.
+        return x;
       }
     } else {
       return x;
+    }
+  }
+
+  virtual StmtRef mutate_stmt_(StmtConditionalLoopRef x) override final {
+    push_scope(true);
+    x = Mutator::mutate_stmt_(x);
+    ScopeRecord scope = pop_scope();
+    if (scope.prelude.empty()) {
+      return x;
+    } else {
+      scope.prelude.emplace_back(x);
+      return new StmtBlock(std::move(scope.prelude));
     }
   }
 
@@ -100,11 +137,11 @@ struct CtrlflowStmt2ExprMutator : public Mutator {
     std::vector<StmtRef> out_stmts;
     for (StmtRef stmt : x->stmts) {
       if (stmt->is<StmtStore>()) {
-        StmtStoreRef stmt2 = stmt;
+        StmtStoreRef stmt2 = mutate_stmt(stmt);
         if (stmt2->dst_ptr->is<MemoryFunctionVariable>()) {
           get_scope().set_func_var(stmt2->dst_ptr, mutate_expr(stmt2->value));
         } else {
-          out_stmts.emplace_back(mutate_stmt(stmt));
+          out_stmts.emplace_back(stmt);
         }
       } else if (stmt->is<StmtConditionalBranch>()) {
         StmtConditionalBranchRef stmt2 = stmt;
@@ -140,11 +177,12 @@ struct CtrlflowStmt2ExprMutator : public Mutator {
 
         if (!then_block->is<StmtNop>() || !else_block->is<StmtNop>()) {
           StmtRef branch = new StmtConditionalBranch(cond, then_block, else_block);
-          out_stmts.emplace_back(mutate_stmt_(branch));
+          out_stmts.emplace_back(mutate_stmt(branch));
         }
 
+      } else {
+        out_stmts.emplace_back(mutate_stmt(stmt));
       }
-
     }
 
     if (out_stmts.empty()) {
